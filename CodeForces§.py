5c025bee0536defed21b21f80c8137dfe1c67b97
abdelrahmanmoussa <moussaabdelrahman17@gmail.com>
2025-03-13 14:15:59 +0100

    https://codeforces.com/problemset/problem/282/A

---
+++ CodeForces§.py
@@ -0,0 +1,26 @@
+#First solutin
+n = int(input())
+y = 0
+
+for _ in range(n):
+    x = input().strip()
+
+    if "++" in x:
+        y += 1
+    elif "--" in x:
+        y -= 1
+
+print(y)
+# Second solution
+n = int(input())
+y = 0
+
+for _ in range(n):
+    x = input().strip()
+
+    if "++" in x:
+        y += 1
+    elif "--" in x:
+        y -= 1
+
+print(y)
